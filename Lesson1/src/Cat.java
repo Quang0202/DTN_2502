2e2c3bca1b8aea190dc3963e50ab7c802c92b2ba
quang.do <quang.dovan0202@gmail.com>
2025-03-23 21:04:30 +0700

    Lesson1 + Lesson2

---
+++ Lesson1/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    String mauLong;
+    String loai;
+    int soTai;
+    int soTuoi;
+    Gender gender;
+
+    public void keu(){
+        System.out.println(loai+ " meo meo .....");
+    }
+}
