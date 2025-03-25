4d2d4703bc75dca89fdf11f33a38b7c985e87313
CAO816 <caothithuthuy00000@gmail.com>
2025-03-25 17:58:54 +0700

    Exercise 1
    Resubmit Exercise 1 of TestingSystem

---
+++ Cao_Thi_Thu_Thuy/TestingSystem/src/com/company/exercise/Exercise2.java
@@ -0,0 +1,11 @@
+package com.company.exercise;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        q1();
+    }
+    public static void q1() {
+        int a = 5;
+        System.out.printf("%d", a);
+    }
+}
