ae0c5e00f166951db5127ad78ec810f4fe5e7ae6
JONATHANCRY <thieuprono1@gmail.com>
2025-03-27 19:49:09 +0700

    push assignment 1 2 3

---
+++ ba_thieu/assignment 123/src/main/java/org/example/Answer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Answer {
+    public int id;
+    public String content;
+    public Question question;
+    public Boolean isCorrect;
+}
---
+++ ba_thieu/assignment 123/src/main/java/org/example/CategoryQuestion.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class CategoryQuestion {
+    public int id;
+    public int name;
+}
