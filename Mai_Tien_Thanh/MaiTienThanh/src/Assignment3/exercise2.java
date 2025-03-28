c956024f7bd9e830c73779bf8e4a0a9b03c07fe7
ThanhMike0501 <stormbaox@gmail.com>
2025-03-28 12:06:26 +0700

    Mai_Tien_Thanh

---
+++ Mai_Tien_Thanh/MaiTienThanh/src/Assignment3/exercise2.java
@@ -0,0 +1,12 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        int[] new_array = new int[6];
+        for(int i = 1;i < new_array.length;i++) {
+            System.out.println("Email: 'Email "+ i + "'" );
+            System.out.println("Username: 'Username "+ i + "'");
+            System.out.println("FullName: 'FullName "+ i + "'");
+            System.out.println("CreateDate: now\n");
+
+        }
+    }
+}
