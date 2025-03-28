19f5e75dcdfc90bf530cf0fca150f93cea57c0a1
trung <trungnguyendn91@gmail.com>
2025-03-28 18:36:57 +0700

    add lession3

---
+++ Nguyen_Thanh_Kien/lession3/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        question1();
+    }
+
+    public static void question1() {
+        float salary = 5240.5F;
+        float salary2 = 10970.055F;
+        System.out.println(Math.round(salary));
+        System.out.println(Math.round(salary2));
+    }
+}
