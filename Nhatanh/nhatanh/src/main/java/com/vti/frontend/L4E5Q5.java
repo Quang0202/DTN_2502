e376e074536894d5f194e825248a19218f52259b
Your Name <you@example.com>
2025-04-04 10:16:11 +0700

    commit

--- Nhatanh/nhatanh/src/main/java/com/vti/frontend/L4E5Q5.java
+++ Nhatanh/nhatanh/src/main/java/com/vti/frontend/L4E5Q5.java
@@ -1,6 +1,9 @@
 package com.vti.frontend;
 
-import com.vti.backend.*;
+import com.vti.backend.Bao;
+import com.vti.backend.QLTL;
+import com.vti.backend.Sach;
+import com.vti.backend.TapChi;
 
 import java.text.ParseException;
 import java.util.Scanner;
@@ -8,7 +11,7 @@ import java.util.Scanner;
 public class L4E5Q5 {
     public static void main(String[] args) throws ParseException {
         Scanner scanner = new Scanner(System.in);
-        QLCB qlcb = new QLCB();
+        QLTL qlcb = new QLTL();
         boolean validResponse = true;
         while (validResponse) {
             System.out.println("mời bạn nhập vào chức năng muốn sử dụng");
@@ -20,56 +23,70 @@ public class L4E5Q5 {
             int inputRequestNumber = scanner.nextInt();
             switch (inputRequestNumber) {
                 case 1:
-                    System.out.println("Nhập tên: ");
-                    String inputName = scanner.next();
-                    System.out.println("Nhập tuổi: ");
-                    int inputAge = scanner.nextInt();
-                    System.out.println("Nhập giới tính (MALE/FEMALE): ");
-                    Gender inputGender = Gender.valueOf(scanner.next());
-                    System.out.println("Nhập địa chỉ: ");
-                    String inputAddress = scanner.next();
-                    System.out.println("Chọn vị trí: \n1.Công nhân\n2.Kỹ sư\n3.Nhân viên");
+                    String inputMaTaiLieu = "";
+                    while (true) {
+                        System.out.println("Nhập Mã tài liệu: ");
+                        inputMaTaiLieu = scanner.next();
+                        if (!qlcb.checkIfIdExist(inputMaTaiLieu)) {
+                            break;
+                        }
+                    }
+                    System.out.println("Nhập Tên nhà xuất bản: ");
+                    String inputPublisher = scanner.next();
+                    System.out.println("Nhập số bản phát hành: ");
+                    int inputPublishNumber = scanner.nextInt();
+                    System.out.println("Chọn loại tài liệu: \n1.Sách\n2.Tạp chí\n3.Báo");
                     int inputPosition = scanner.nextInt();
                     switch (inputPosition) {
                         case 1:
-                            boolean validInputLv = true;
-                            int inputLv = 0;
-                            while (validInputLv) {
-                                System.out.println("Nhập số bậc");
-                                inputLv = scanner.nextInt();
-                                if (inputLv > 0 && inputLv <= 10) {
-                                    validInputLv = false;
-                                }
-                            }
-                            CongNhan newCongNhan = new CongNhan(inputLv, inputName, inputAge, inputGender, inputAddress);
-                            qlcb.themCanBo(newCongNhan);
+                            System.out.println("Nhập Tên tác giả: ");
+                            String inputAuthorName = scanner.next();
+                            System.out.println("Nhập số trang: ");
+                            int inputPageNum = scanner.nextInt();
+                            Sach newSach = new Sach(inputMaTaiLieu, inputPublisher, inputPublishNumber, inputAuthorName, inputPageNum);
+                            qlcb.themTaiLieu(newSach);
                             break;
                         case 2:
-                            System.out.println("Nhập ngành đào tạo");
-                            String inputNganhDaoTao = scanner.next();
-                            KySu newKySu = new KySu(inputNganhDaoTao, inputName, inputAge, inputGender, inputAddress);
-                            qlcb.themCanBo(newKySu);
+                            System.out.println("Nhập Số phát hành: ");
+                            int inputPublishNum = scanner.nextInt();
+                            System.out.println("Nhập tháng phát hành: ");
+                            int inputPublishMonth = scanner.nextInt();
+                            TapChi newTapChi = new TapChi(inputMaTaiLieu, inputPublisher, inputPublishNumber, inputPublishNum, inputPublishMonth);
+                            qlcb.themTaiLieu(newTapChi);
                             break;
                         case 3:
-                            System.out.println("Nhập công việc");
-                            String inputCongViec = scanner.next();
-                            NhanVien newNhanVien = new NhanVien(inputCongViec, inputName, inputAge, inputGender, inputAddress);
-                            qlcb.themCanBo(newNhanVien);
+                            System.out.println("Nhập Ngày phát hành: ");
+                            int inputPublishDate = scanner.nextInt();
+                            Bao newBao = new Bao(inputMaTaiLieu, inputPublisher, inputPublishNumber, inputPublishDate);
+                            qlcb.themTaiLieu(newBao);
+                            break;
+                        default:
+                            System.out.println("nhập sai");
                             break;
                     }
                     break;
-                case 2:
-                    System.out.println("Nhập tên cán bộ cần tìm: ");
-                    String inputSearchQuery = scanner.next();
-                    qlcb.timCanBo(inputSearchQuery);
+                case 4:
+                    System.out.println("Nhập Mã tài liệu cần xoá: ");
+                    inputMaTaiLieu = scanner.nextLine();
+                    if (qlcb.checkIfIdExist(inputMaTaiLieu)) {
+                        qlcb.xoaTaiLieu(inputMaTaiLieu);
+                    } else {
+                        System.out.println("Tài liệu cần xoá không tồn tại");
+                    }
                     break;
                 case 3:
-                    qlcb.showCanBo();
+                    System.out.println("Nhập Mã tài liệu cần hiển thị: ");
+                    inputMaTaiLieu = scanner.next();
+                    if (qlcb.checkIfIdExist(inputMaTaiLieu)) {
+                        qlcb.showTaiLieu(inputMaTaiLieu);
+                    } else {
+                        System.out.println("Tài liệu cần xoá không tồn tại");
+                    }
                     break;
-                case 4:
-                    System.out.println("Nhập tên cán bộ cần xoá: ");
-                    String inputDeleteQuery = scanner.next();
-                    qlcb.deleteCanBo(inputDeleteQuery);
+                case 2:
+                    System.out.println("Nhập loại tài liệu cần tìm:\n1.Sách\n2.Tạp chí\n3.Báo");
+                    int inputFilterQuery = scanner.nextInt();
+                    qlcb.findByTypeTaiLieu(inputFilterQuery);
                     break;
                 case 5:
                     validResponse = false;
