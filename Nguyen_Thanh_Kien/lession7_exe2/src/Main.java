6796b99ef8dbca8eccaa103997950908a82f6d2e
trung <trungnguyendn91@gmail.com>
2025-04-04 18:12:41 +0700

    add lession 7 exe 2

--- Nguyen_Thanh_Kien/lession7_exe2/src/Main.java
+++ Nguyen_Thanh_Kien/lession7_exe2/src/Main.java
@@ -1,7 +1,15 @@
+import model.PrimaryStudent;
+import model.Student;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+//        System.out.printf("%.2f\n", MyClass.sum(3));
+
+//        Student student = new Student(1, "test");
+
+        Student primary = new PrimaryStudent(2,"Kien");
+        primary.study();
     }
 }
