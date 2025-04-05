b16fc32fe6d680d248a71a6bd8bd49005e79b8bf
trung <trungnguyendn91@gmail.com>
2025-04-05 20:53:23 +0700

    add lession 7 question 1-7

--- Nguyen_Thanh_Kien/lession7_exe3/src/Main.java
+++ Nguyen_Thanh_Kien/lession7_exe3/src/Main.java
@@ -1,15 +1,32 @@
+import utils.FileManager;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static final String pathFolderName = "\\src\\document\\";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) throws IOException {
+//        question 1
+        System.out.println(FileManager.isFileExist(pathFolderName + "\\\\test.txt"));
+
+//        question 2
+//        FileManager.createNewFile(pathFolderName + "\\\\test.txt");
+
+//        question 4
+//        FileManager.deleteFile(pathFolderName + "test.txt");
+
+//        question 5
+//        System.out.println(FileManager.isFolder(pathFolderName + "\\\\test.txt"));
+
+//        question 6
+//        for (String s : FileManager.getAllFileName(pathFolderName)) {
+//            System.out.println(s);
+//        }
+
+//        question 7
+//        FileManager.copyFile(pathFolderName + "\\\\test.txt\\", "\\\\src\\document2\\abc.txt");
+//        FileManager.copyFile(pathFolderName + "test.txt", "\\src\\document2", "xyz.txt");
     }
 }
