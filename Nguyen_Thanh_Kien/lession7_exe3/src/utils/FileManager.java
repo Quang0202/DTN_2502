c6d68a6f00548574ce738f8e0648d6de0ce40586
trung <trungnguyendn91@gmail.com>
2025-04-07 13:33:17 +0700

    fix

--- Nguyen_Thanh_Kien/lession7_exe3/src/utils/FileManager.java
+++ Nguyen_Thanh_Kien/lession7_exe3/src/utils/FileManager.java
@@ -186,49 +186,10 @@ public class FileManager {
         String[] s = fileLink.split("/");
         String name = s[s.length - 1];
         String pathFolder = rootPath + formatPath(folder) + "\\" + name;
-        String nameNotExtension = removeExtension(name);
-        String typeExtension = name.substring(name.lastIndexOf("."));
         File file = new File(pathFolder);
         if (file.exists()) {
-            List<String> listFile = getAllFileName(folder);
-            for (int i = 0; i < listFile.size(); i++) {
-                listFile.set(i, removeExtension(listFile.get(i)));
-            }
-            listFile = listFile.stream()
-                    .filter(fileName -> fileName.equals(nameNotExtension) ||
-                            (fileName.startsWith(nameNotExtension) && checkNameFileValid(fileName))
-                    ).collect(Collectors.toList());
-
-            if (!listFile.isEmpty()) {
-                if (listFile.size() == 1) {
-                    pathFolder = nameNotExtension + " (2)" + typeExtension;
-                } else {
-                    listFile.sort(new Comparator<String>() {
-                        @Override
-                        public int compare(String o1, String o2) {
-                            return getNumberOfFileByFileName(o1) - getNumberOfFileByFileName(o2);
-                        }
-                    });
-                    boolean check = false;
-                    for (int i = 0; i < listFile.size(); i++) {
-                        if (getNumberOfFileByFileName(listFile.get(i)) != 0 && i == 0) {
-                            pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + typeExtension;
-                            break;
-                        }
-                        if (getNumberOfFileByFileName(listFile.get(i)) != (i + 1) && i != 0) {
-                            pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + " (" + (i + 1) + ")" + typeExtension;
-                            check = true;
-                            break;
-                        }
-                    }
-                    if (!check) {
-                        pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + " (" + (listFile.size() + 1) + ")" + typeExtension;
-                    }
-                }
-            }
+            pathFolder = getNameFileDuplicate(folder, removeExtension(name), name.substring(name.lastIndexOf(".")));
         }
-
-
         URL url = new URL(fileLink);
         URLConnection connection = url.openConnection();
         int size = connection.getContentLength();
@@ -248,24 +209,26 @@ public class FileManager {
         System.out.println("Download successfully");
     }
 
-    private static Boolean checkNameFileValid(String fileName) {
-        if (fileName.contains(" (") && fileName.charAt(fileName.length() - 1) == ')') {
-            fileName = fileName.substring(fileName.lastIndexOf(" ("));
-            if (fileName.length() > 3) {
-                try {
-                    Integer.parseInt(fileName.substring(2, fileName.lastIndexOf(")")));
-                    return true;
-                } catch (NumberFormatException e) {
-                    return false;
-                }
+    private static Boolean checkNameFileValid(String fileName, String nameNotExtension) {
+        fileName = fileName.substring(fileName.indexOf(nameNotExtension) + nameNotExtension.length());
+        if (fileName.isEmpty()) {
+            return true;
+        } else if (fileName.length() > 3 && fileName.startsWith(" (") && fileName.endsWith(")")) {
+            try {
+                Integer.parseInt(fileName.substring(2, fileName.lastIndexOf(")")));
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
             }
-            return false;
         }
         return false;
     }
 
     private static String removeExtension(String fileName) {
-        return fileName.substring(0, fileName.lastIndexOf("."));
+        if(fileName.contains(".")) {
+            return fileName.substring(0, fileName.lastIndexOf("."));
+        }
+        return fileName;
     }
 
     private static Integer getNumberOfFileByFileName(String fileName) {
@@ -274,5 +237,78 @@ public class FileManager {
         }
         fileName = fileName.substring(fileName.lastIndexOf("(") + 1, fileName.length() - 1);
         return Integer.parseInt(fileName);
+    }
+
+    private static String getNameFileDuplicate(String folder, String nameNotExtension, String typeExtension) {
+        String pathFolder = "";
+        //            Lấy ra toàn bô file trong folder
+        List<String> listFile = getAllFileName(folder);
+//        duyệt for để lây ra những tên có phần mở rộng và tên tương ứng
+//        Ví dụ danh sách có a (1).txt, a (3).txt, a.txt, b(1).txt tên cần tìm là a
+//        sẽ trả về danh sách có a (1), a(3), a. Xóa phần mở rộng và tên không hợp lệ khỏi danh sách
+        for (int i = 0; i < listFile.size(); i++) {
+//                Lấy ra tên file không chứa phần mở rộng
+            String tempName = removeExtension(listFile.get(i));
+//                Kiểm tra nếu tên file bắt đầu bằng tên file được lấy từ url và có chứa .
+            if (tempName.startsWith(nameNotExtension) &&  listFile.get(i).contains(".")) {
+//                  Lấy ra kiểu mở rộng của tên file trong danh sách file
+                String tempTypeExtension = listFile.get(i).substring(listFile.get(i).lastIndexOf("."));
+//              DK1:  Nếu tên file bằng tên được lấy từ url và phần mở rộng file bằng phần mở rộng của url
+//              DK2:  Nếu tên file có chứa kí tự ( ) và số nằm trong đó, và phần mở rộng file bằng phần mở rộng của url
+                if ((tempName.equals(nameNotExtension) || checkNameFileValid(tempName, nameNotExtension))
+                        && tempTypeExtension.equals(typeExtension)) {
+//              list có tên a.txt,a (1).txt, b.txt ở đây sẽ xử lý lấy ra a.txt và a (1).txt sau đó remove name ko hợp lệ
+                    listFile.set(i,tempName);
+                } else {
+                    // remove những name không thoả mãn sau đó gán lại i để tránh sót dữ liệu
+                    listFile.remove(i);
+                    i--;
+                }
+            } else {
+//                    remove những name không bắt đầu bằng tên lấy từ url và không chứa phần mở rộng
+//                    sau đó gán lại i để tránh sót dữ liệu
+                listFile.remove(i);
+                i--;
+            }
+        }
+
+        if (!listFile.isEmpty()) {
+//                check truờng hợp list có đúng 1 tên mà tên đó bằng hoặc không bằng với tên lấy từ url
+            if (listFile.size() == 1) {
+                if(listFile.get(0).equals(nameNotExtension)){
+//                        nếu bằng thì thêm (2) vào tên file
+                    pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + " (2)" + typeExtension;
+                } else {
+//                        nếu không bằng thì set bằng tên lấy từ url
+                    pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + typeExtension;
+                }
+            } else  {
+                listFile.sort(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return getNumberOfFileByFileName(o1) - getNumberOfFileByFileName(o2);
+                    }
+                });
+                boolean check = false;
+                if(!listFile.get(0).equals(nameNotExtension)){
+                    pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + typeExtension;
+                }
+                for (int i = 1; i < listFile.size(); i++) {
+//                        lấy ra số của tên file a (2) sẽ trả về 2, sau đó kiểm tra để gán số hợp lệ
+//                        kiểm tra trong trường hợp có 1 file bị xóa mất chuỗi liên tục ví dụ đã có
+//                        a (1) a (3) thì gán tên tiếp theo là a (2)
+                    if (getNumberOfFileByFileName(listFile.get(i)) != (i + 1)) {
+                        pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + " (" + (i + 1) + ")" + typeExtension;
+                        check = true;
+                        break;
+                    }
+                }
+//                    trường hợp không bị mất chuỗi liên tục thì set tên cộng thêm số độ dài danh sách + 1
+                if (!check) {
+                    pathFolder = rootPath + formatPath(folder) + "\\" + nameNotExtension + " (" + (listFile.size() + 1) + ")" + typeExtension;
+                }
+            }
+        }
+        return pathFolder;
     }
 }
