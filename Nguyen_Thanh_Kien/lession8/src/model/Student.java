2fa3f037101b42e0a21348e1ca69389c9e45c5fa
trung <trungnguyendn91@gmail.com>
2025-04-08 21:01:43 +0700

    add lession8

---
+++ Nguyen_Thanh_Kien/lession8/src/model/Student.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Student {
+    private static int countId = 0;
+    private int id;
+    private String name;
+
+    public static int getCountId() {
+        return countId;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(String name) {
+        countId++;
+        this.id = countId;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
