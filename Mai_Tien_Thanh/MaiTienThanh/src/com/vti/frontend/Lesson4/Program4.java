3191bf5a9e3a5563762a5e3ecc5c9fc21bd467c7
ThanhMike0501 <stormbaox@gmail.com>
2025-04-12 04:46:27 +0700

    Mai_Tien_Thanh

--- Mai_Tien_Thanh/MaiTienThanh/src/com/vti/frontend/Lesson4/Program4.java
+++ Mai_Tien_Thanh/MaiTienThanh/src/com/vti/frontend/Lesson4/Program4.java
@@ -1,7 +1,11 @@
 package MaiTienThanh.src.com.vti.frontend.Lesson4;
 
+import MaiTienThanh.src.com.vti.backend.Lesson4.Exercise4;
+
 public class Program4 {
     public static void main(String[] args) {
+        Exercise4 ex = new Exercise4();
+        ex.question1();
 
     }
 }
