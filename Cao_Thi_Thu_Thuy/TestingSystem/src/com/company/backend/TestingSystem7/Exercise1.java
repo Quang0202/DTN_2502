a33d8070b86c5c2c220d3a89db3587434f6496a2
quang.do <quang.dovan0202@gmail.com>
2025-04-18 19:05:43 +0700

    Lesson8

--- Cao_Thi_Thu_Thuy/TestingSystem/src/com/company/backend/TestingSystem7/Exercise1.java
+++ Cao_Thi_Thu_Thuy/TestingSystem/src/com/company/backend/TestingSystem7/Exercise1.java
@@ -17,24 +17,37 @@ public class Exercise1 {
     }
     public void q2(List<Student> students){
         // B1.
-        for(Student s: students){
-            s.nopQuy(100);
+        for(Student student : students){
+            student.nopQuy(100);
         }
-        Student.printMoneyGroup("B1");
-        // B2.
+        System.out.println("Số quỹ còn lại là: "+ Student.getMoneyGroup());
+
+//        Student.setMoneyGroup(Student.getMoneyGroup() + students.size() * 100);
+//        Student.printMoneyGroup("B1");
+//        // B2.
         students.get(0).layQuy(50);
-        Student.printMoneyGroup("B2");
-        // B3.
-        students.get(0).layQuy(20);
-        Student.printMoneyGroup("B3");
-        // B4.
-        students.get(0).layQuy(150);
-        Student.printMoneyGroup("B4");
-        // B5.
-        for(Student s: students){
-            s.nopQuy(50);
+        System.out.println("Số quỹ còn lại là: "+ Student.getMoneyGroup());
+
+//        Student.setMoneyGroup(Student.getMoneyGroup() - 50);
+//        Student.printMoneyGroup("B2");
+//        // B3.
+        students.get(1).layQuy(20);
+        System.out.println("Số quỹ còn lại là: "+ Student.getMoneyGroup());
+//        Student.setMoneyGroup(Student.getMoneyGroup() - 20);
+//        Student.printMoneyGroup("B3");
+//        // B4.
+        students.get(2).layQuy(150);
+        System.out.println("Số quỹ còn lại là: "+ Student.getMoneyGroup());
+//        Student.setMoneyGroup(Student.getMoneyGroup() - 150);
+//        Student.printMoneyGroup("B4");
+//        // B5.
+        for(Student student : students){
+            student.nopQuy(50);
         }
-        Student.printMoneyGroup("B5");
+        System.out.println("Số quỹ còn lại là: "+ Student.getMoneyGroup());
+//        Student.setMoneyGroup(Student.getMoneyGroup() + students.size() * 50);
+//        Student.printMoneyGroup("B5");
+
     }
     public void q3(){
         System.out.println("Max: " + MyMath.max(3.4, 5.0));
@@ -42,36 +55,49 @@ public class Exercise1 {
         System.out.println("Sum: " + MyMath.sum(3.1, 2.9));
     }
     public void q5(){
-        Student s1 = new PrimaryStudent(1, "An");
-        Student s2 = new PrimaryStudent(2, "Bình");
-        Student s3 = new SecondaryStudent(3, "Cường");
-        Student s4 = new SecondaryStudent(4, "Dũng");
-        Student s5 = new SecondaryStudent(5, "Em");
-        Student s6 = new SecondaryStudent(6, "Phúc");
+        try {
+            Student s1 = new PrimaryStudent(1, "An");
+            Student s2 = new PrimaryStudent(2, "Bình");
+            Student s3 = new SecondaryStudent(3, "Cường");
+            Student s4 = new SecondaryStudent(4, "Dũng");
+            Student s5 = new SecondaryStudent(5, "Em");
+            Student s6 = new SecondaryStudent(6, "Phúc");
 
-        System.out.println("\n--- Tổng kết ---");
-        System.out.println("Số lượng Student: " + Student.getCountStudent());
-        System.out.println("Số lượng PrimaryStudent: " + PrimaryStudent.getCountPrimaryStudent());
-        System.out.println("Số lượng SecondaryStudent: " + SecondaryStudent.getCountSecondaryStudent());
+            System.out.println("\n--- Tổng kết ---");
+            System.out.println("Số lượng Student: " + Student.getCountStudent());
+            System.out.println("Số lượng PrimaryStudent: " + PrimaryStudent.getCountPrimaryStudent());
+            System.out.println("Số lượng SecondaryStudent: " + SecondaryStudent.getCountSecondaryStudent());
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
     public void q6(){
-        Student s1 = new PrimaryStudent(1, "An");
-        Student s2 = new PrimaryStudent(2, "Bình");
-        Student s3 = new SecondaryStudent(3, "Cường");
-        Student s4 = new SecondaryStudent(4, "Dũng");
-        Student s5 = new SecondaryStudent(5, "Em");
-        Student s6 = new SecondaryStudent(6, "Phúc");
-        Student s7 = new SecondaryStudent(7, "Em");
-        Student s8 = new SecondaryStudent(8, "Phúc");
+        try {
+            Student s1 = new PrimaryStudent(1, "An");
+            Student s2 = new PrimaryStudent(2, "Bình");
+            Student s3 = new SecondaryStudent(3, "Cường");
+            Student s4 = new SecondaryStudent(4, "Dũng");
+            Student s5 = new SecondaryStudent(5, "Em");
+            Student s6 = new SecondaryStudent(6, "Phúc");
+            Student s7 = new SecondaryStudent(7, "Em");
+            Student s8 = new SecondaryStudent(8, "Phúc");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println(Student.getCountStudent());
     }
-    public void q8() throws HinhHocException {
-        HinhHoc h1 = new HinhChuNhat(2, 3);
-        HinhHoc h2 = new HinhChuNhat(2, 3);
-        HinhHoc h3 = new HinhChuNhat(2, 3);
-        HinhHoc h4 = new HinhChuNhat(2, 3);
-        HinhHoc h5 = new HinhChuNhat(2, 3);
-        HinhHoc h6 = new HinhTron(2);
-        HinhHoc h7 = new HinhTron(2);
-        System.out.println(Configs.SO_LUONG_HINH_TOI_DA);
+    public void q8() {
+        try {
+            HinhHoc h1 = new HinhChuNhat(2, 3);
+            HinhHoc h2 = new HinhChuNhat(2, 3);
+            HinhHoc h3 = new HinhChuNhat(2, 3);
+            HinhHoc h4 = new HinhChuNhat(2, 3);
+            HinhHoc h5 = new HinhChuNhat(2, 3);
+            HinhHoc h6 = new HinhTron(2);
+            HinhHoc h7 = new HinhTron(2);
+        } catch (HinhHocException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println(HinhHoc.getCount());
     }
 }
--- MainProject/Lesson7/src/FileProgram.java
+++ MainProject/Lesson7/src/FileProgram.java
@@ -1,13 +1,20 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileProgram {
     public static void main(String[] args) {
-        File file = new File("/Users/doquang/Documents");
+        File file = new File("/Users/doquang/Documents/Test/test.ser");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+
+        }
 //        if(!file.exists()){
 //            try {
 //                file.createNewFile();
@@ -15,27 +22,27 @@ public class FileProgram {
 //            } catch (IOException e) {
 //                System.out.println("Create file error");
 //            }
-////            file.mkdir();
+//           file.mkdir();
 //        }else {
 //            file.delete();
 //            System.out.println("delete success!");
 //        }
 
-        if(file.isFile()){
-            System.out.println("Is file");
-        }
+//        if(file.isFile()){
+//            System.out.println("Is file");
+//        }
 
 //        String [] fileName = file.list();
 //        for(String name : fileName){
 //            System.out.println(name);
 //        }
-
-        File [] files = file.listFiles();
-        for(File f : files){
-            if(f.isFile()) {
-                System.out.println(f.getName());
-            }
-        }
+//
+//        File [] files = file.listFiles();
+//        for(File f : files){
+//            if(f.isFile()) {
+//                System.out.println(f.getName());
+//            }
+//        }
 
 
         //Rename file
@@ -48,16 +55,70 @@ public class FileProgram {
 //            System.out.println("error");
 //        }
         //Move file
-        Path path1 = Path.of("/Users/doquang/Documents/test2.txt");
-        Path path2 = Path.of("/Users/doquang/Documents/JavaCore/test2.txt");
+//        Path path1 = Path.of("/Users/doquang/Documents/test2.txt");
+//        Path path2 = Path.of("/Users/doquang/Documents/JavaCore/test2.txt");
+//
+//        try {
+//            Files.move(path1, path2, StandardCopyOption.REPLACE_EXISTING);
+//            System.out.println("move success!");
+//        } catch (IOException e) {
+//            System.out.println("move error!");
+//        }
 
-        try {
-            Files.move(path1, path2, StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("move success!");
-        } catch (IOException e) {
-            System.out.println("move error!");
+//        Read data from file
+//        FileInputStream fileInputStream = new FileInputStream("/Users/doquang/Documents/Test/test.txt");
+//
+//        byte[] bytes = new byte[4];
+//
+//        int length = fileInputStream.read(bytes);
+//        String result= "";
+//        while(length > -1){
+//            String str = new String(bytes,0,length);
+//            result+=str;
+//            length = fileInputStream.read(bytes);
+//        }
+//        System.out.println(result);
+//        try(FileOutputStream fileOutputStream = new FileOutputStream("/Users/doquang/Documents/Test/test.txt",true);
+//        ) {
+//            String outStr = "Nguyen van a";
+//            byte[] outBytes = outStr.getBytes();
+//            fileOutputStream.write(outBytes);
+//            System.out.println("Write success");
+//        } catch (IOException e){
+//
+//        }
+
+//         Read object
+
+//        Department department = new Department(1, "sale");
+//        Department department2 = new Department(2, "marketing");
+//        Department department3 = new Department(3, "CEO");
+//        List<Department> departments = Arrays.asList(department3,department2,department);
+//
+//        try(FileOutputStream fileOutputStream = new FileOutputStream("/Users/doquang/Documents/Test/test.ser");
+//            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)
+//        ) {
+//            objectOutputStream.writeObject(departments);
+//            System.out.println("Write list department success");
+//
+//        } catch (Exception e) {
+//            System.out.println("error");
+//        }
+
+        try (FileInputStream fileInputStream = new FileInputStream("/Users/doquang/Documents/Test/test.ser");
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)
+        ) {
+
+//            Department department = (Department) objectInputStream.readObject();
+//            System.out.println(department);
+            List<Department> departments = (List<Department>) objectInputStream.readObject();
+            for(Department department : departments){
+                System.out.println(department);
+            }
+
+        } catch (Exception e) {
+            System.out.println("error");
         }
-
 
     }
 }
