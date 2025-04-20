f6340b1872a0616ae79b181170e280209bd5f192
PhamAnhTuan121 <phamtuan785aa@gmail.com>
2025-04-20 20:43:33 +0700

    Assignment7And8

---
+++ Pham_Anh_Tuan/Assignment8/Assignment8/src/fontend/Program4.java
@@ -0,0 +1,14 @@
+package fontend;
+
+import backend.Exercise4;
+
+public class Program4 {
+    public static void main(String[] args) {
+        Exercise4 ex4 = new Exercise4();
+        ex4.question1();
+        ex4.question2();
+        ex4.question3();
+        ex4.question4();
+        ex4.question5();
+    }
+}
