75e2593f7782dd134fee4821de669f1ddc799f5b
ThanhMike0501 <stormbaox@gmail.com>
2025-04-21 12:17:00 +0700

    Mai_Tien_Thanh

---
+++ Mai_Tien_Thanh/MaiTienThanh/src/com/vti/backend/Lesson6/Exercise1/Question2/entity/GioiTinh.java
@@ -0,0 +1,5 @@
+package MaiTienThanh.src.com.vti.backend.Lesson6.Exercise1.Question2.entity;
+
+public enum GioiTinh {
+    NAM, NU, KHONGBIET;
+}
---
+++ Mai_Tien_Thanh/MaiTienThanh/src/com/vti/backend/Lesson6/Exercise1/Question2/frontend/Program.java
@@ -0,0 +1,10 @@
+package MaiTienThanh.src.com.vti.backend.Lesson6.Exercise1.Question2.frontend;
+
+import MaiTienThanh.src.com.vti.backend.Lesson6.Exercise1.Question2.backend.Exercise5;
+
+public class Program {
+	public static void main(String[] args) {
+		Exercise5 exercise5 = new Exercise5();
+		exercise5.question1_2();
+	}
+}
