39ba53605f9b79b27e93633c5425d03fd8e83f8d
trung <trungnguyendn91@gmail.com>
2025-04-23 16:37:29 +0700

    fix error sonar

--- Nguyen_Thanh_Kien/springboot/AccountManagement/src/main/java/vti/accountmanagement/utils/MessageUtil.java
+++ Nguyen_Thanh_Kien/springboot/AccountManagement/src/main/java/vti/accountmanagement/utils/MessageUtil.java
@@ -1,6 +1,5 @@
 package vti.accountmanagement.utils;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
