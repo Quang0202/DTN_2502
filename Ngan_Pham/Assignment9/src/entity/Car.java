2bfcefdd63829291aa32ad4cd1e6b01e9e125a6d
andypham67777 <nganpham1303@gmail.com>
2025-04-23 10:33:13 +0700

    A9

---
+++ Ngan_Pham/Assignment9/src/entity/Car.java
@@ -0,0 +1,31 @@
+package entity;
+
+public class Car {
+    private String name;
+    private String type;
+
+    public Car(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public class Engine {
+        private String engineType;
+
+        public String getEngineType() {
+            return engineType;
+        }
+
+        public void setEngineType(String engineType) {
+            this.engineType = engineType;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
