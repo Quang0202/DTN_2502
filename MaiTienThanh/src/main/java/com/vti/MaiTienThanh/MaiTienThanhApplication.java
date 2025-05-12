7cff4e657bcb2e76fd57e7a94e361eeb54ee19d9
ThanhMike0501 <stormbaox@gmail.com>
2025-05-12 20:04:11 +0700

    MaiTienThanh

---
+++ MaiTienThanh/src/main/java/com/vti/MaiTienThanh/MaiTienThanhApplication.java
@@ -0,0 +1,13 @@
+package com.vti.MaiTienThanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiTienThanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiTienThanhApplication.class, args);
+	}
+
+}
