e0ddfe50ebd3da3dd0f8166d7646d9e780b2babf
PhamAnhTuan121 <phamtuan785aa@gmail.com>
2025-05-20 20:38:21 +0700

    Assignment3And4
    Assignment 3 + 4

---
+++ Pham_Anh_Tuan/SpringBoot/Assignment3/Assignment3/src/main/java/com/vti/assignment3/entity/PositionName.java
@@ -0,0 +1,5 @@
+package com.vti.assignment3.entity;
+
+public enum PositionName {
+    Dev, Test, Scrum_Master, PM
+}
---
+++ Pham_Anh_Tuan/SpringBoot/Assignment3/Assignment3/src/main/java/com/vti/assignment3/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.vti.assignment3.repository;
+
+import com.vti.assignment3.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Integer> {
+}
